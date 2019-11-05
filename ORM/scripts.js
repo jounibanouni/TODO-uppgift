11f5da84a42632bce028e0742eb56a81665cdd63
Jonas Eriksson <45967865+jounibanouni@users.noreply.github.com>
2019-11-05 08:53:59 +0100

    most recent update
    full functionality

--- ORM/scripts.js
+++ ORM/scripts.js
@@ -13,19 +13,19 @@ var TODOStorage = (function () {
     }
 
     //Create TODO
-    function createNewTodo(userid, done, description){
+    function createNewTodo(userid, done, description) {
         //Get maxID in todos
         let maxId = 0;
-        for(const i in todos){
+        for (const i in todos) {
             const todo = todos[i];
-            if(todo.id > maxId){
+            if (todo.id > maxId) {
                 maxId = todo.id;
             }
         }
         //Create TODO-object
         const todo = {
             userid: userid,
-            id: maxId+1,
+            id: maxId + 1,
             done: done,
             description: description
         }
@@ -37,22 +37,22 @@ var TODOStorage = (function () {
         saveChanges();
     }
 
-    function saveChanges(){
+    function saveChanges() {
         const lsTodos = JSON.stringify(todos);
         localStorage.setItem("TODOS", lsTodos);
     }
 
     //List TODOs
-    function listTodos(){
+    function listTodos() {
         return todos;
     }
 
     //Read TODO by id
-    function getTodoById(id){
-        for(const i in todos){
+    function getTodoById(id) {
+        for (const i in todos) {
             const todo = todos[i];
 
-            if (todo.id === id){
+            if (todo.id === id) {
                 return todo;
             }
         }
@@ -60,12 +60,40 @@ var TODOStorage = (function () {
         return null;
     }
 
+    function removeTodoByCheckbox() {
+
+        for (const i in todos) {
+            const todo = todos[i];
+
+            if (todo.userid === loggedinUserId) {
+
+                if (document.getElementById("checkBox" + todo.id).checked) {
+                    //alert(todo.id);
+                    TODOStorage.deleteTodoById(todo.id);
+                    continue;
+                }
+            }
+
+        }
+
+    }
+
+    function listTodosByUser(loggedinUserId) {
+        const listShowTodos = document.getElementById("show-todos");
+        for (const i in todos) {
+            const todo = todos[i];
+            if (loggedinUserId === todos[i].userid) {
+                listShowTodos.innerHTML += "<li class='list-class'id='listInput" + todo.id + "'><input type='checkbox' id='checkBox" + todo.id + "' >" + todo.description + "</li>";
+            }
+        }
+    }
+
     //Update TODO
-    function updateTodo(id, done, description){
-        for(const i in todos){
+    function updateTodo(id, done, description) {
+        for (const i in todos) {
 
             //Update if id is found
-            if(todos[i].id === id){
+            if (todos[i].id === id) {
                 todos[i].done = done;
                 todos[i].description = description;
             }
@@ -75,11 +103,11 @@ var TODOStorage = (function () {
     }
 
     //Delete TODO
-    function deleteTodoById(id){
-        for(const i in todos){
+    function deleteTodoById(id) {
+        for (const i in todos) {
             const todo = todos[i];
 
-            if(todo.id === id){
+            if (todo.id === id) {
                 todos.splice(i, 1);
                 break;
             }
@@ -88,20 +116,22 @@ var TODOStorage = (function () {
         saveChanges();
     }
 
-    return { 
+    return {
         init,
         createNewTodo,
         listTodos,
         getTodoById,
         updateTodo,
-        deleteTodoById
+        deleteTodoById,
+        listTodosByUser,
+        removeTodoByCheckbox
     }
 
 })();
 
-var UserStorage = (function(){
+var UserStorage = (function () {
     var users = [];
-    
+
 
     function init() {
         const lsUsers = localStorage.getItem("Users");
@@ -112,18 +142,24 @@ var UserStorage = (function(){
         }
     }
 
-    function createNewUser(username, password){
+    function createNewUser(username, password) {
         //Get maxID in todos
+        for (const i in users){
+            if(users[i].username === username){
+                alert("That username is already taken!");
+                return;
+            }
+        }
         let maxId = 0;
-        for(const i in users){
+        for (const i in users) {
             const user = users[i];
-            if(user.id > maxId){
+            if (user.id > maxId) {
                 maxId = user.id;
             }
         }
         //Create TODO-object
         const user = {
-            id: maxId+1,
+            id: maxId + 1,
             username: username,
             password: password
         }
@@ -135,33 +171,43 @@ var UserStorage = (function(){
         saveChanges();
     }
 
-    function saveChanges(){
+    function saveChanges() {
         const lsUsers = JSON.stringify(users);
         localStorage.setItem("Users", lsUsers);
     }
 
-    function showUsers(){
+    function showUsers() {
         return users;
     }
 
-    function checkUser(username, password){
-        for (const i in users){
-            if(username === users[i].username){
-                if(users[i].password === password){
+    //Log in function
+    function checkUser(username, password) {
+        let foundUser = false;
+        for (const i in users) {
+            if (username === users[i].username) {
+                foundUser = true;
+                if (users[i].password === password) {
                     loggedinUserId = users[i].id;
                     loggedInScreen();
-                    hideCreateTodo();
+                    TODOStorage.listTodosByUser(loggedinUserId);
                     alert("Congrats you are now logged in");
                 }
-                else if(users[i].password !== password){
-                    alert("Wrong password, try again");
+                else if (users[i].password !== password) {
+                    alert("Wrong password or username");
                 }
             }
+            if (username !== users[i].username){
+                continue;
+            }
+        }
+
+        if (foundUser === false){
+            alert("Could not find user");
         }
         console.log(loggedinUserId);
     }
 
-    return{
+    return {
         init,
         createNewUser,
         showUsers,
@@ -169,8 +215,8 @@ var UserStorage = (function(){
     }
 })();
 
-var EventHandlers = (function(){
-    function init(){
+var EventHandlers = (function () {
+    function init() {
         const submitNewUser = document.getElementById("submit-user-button");
         submitNewUser.addEventListener("click", createNewUserInfo);
 
@@ -183,33 +229,50 @@ var EventHandlers = (function(){
         const createNewTodoButton = document.getElementById("submit-new-todo");
         createNewTodoButton.addEventListener("click", createNewTodoEntry);
 
+        const clearTodoButton = document.getElementById("clear-todo");
+        clearTodoButton.addEventListener("click", clearTodoById);
+
+        const logoutButton = document.getElementById("logout-button");
+        logoutButton.addEventListener("click", logOutFromPage);
+
         //window.addEventListener("onload", hideCreateTodo)
 
     }
 
-    function createNewUserInfo(){
+    function clearTodoById() {
+        TODOStorage.removeTodoByCheckbox();
+        clearPage();
+
+    }
+
+    function logOutFromPage() {
+        location.reload();
+    }
+
+    function createNewUserInfo() {
         const usernameInput = document.getElementById("username-input");
         const passwordInput = document.getElementById("password-input");
 
         const usernameInputString = usernameInput.value;
         const passwordInputString = passwordInput.value;
 
-        UserStorage.createNewUser(usernameInputString, passwordInputString);
+        UserStorage.createNewUser(usernameInputString.toLowerCase(), passwordInputString);
+        location.reload();
 
     }
 
-    function loginUser(){
+    function loginUser() {
         const usernameLoginInput = document.getElementById("username-login-input");
         const passwordLoginInput = document.getElementById("password-login-input");
 
         const usernameLoginValue = usernameLoginInput.value;
         const passwordLoginvalue = passwordLoginInput.value;
 
-        UserStorage.checkUser(usernameLoginValue, passwordLoginvalue);
+        UserStorage.checkUser(usernameLoginValue.toLowerCase(), passwordLoginvalue);
 
     }
 
-    function createNewUserScreen(){
+    function createNewUserScreen() {
         const loginDivBox = document.getElementById("login-box");
         const createNewUserBox = document.getElementById("create-new-user-box");
 
@@ -218,32 +281,37 @@ var EventHandlers = (function(){
 
     }
 
-    function createNewTodoEntry(){
+    function createNewTodoEntry() {
         const descriptionInput = document.getElementById("new-todo-description");
-        descriptionInputString = descriptionInput.value;
-        if(loggedinUserId !== undefined){
-            TODOStorage.createNewTodo(loggedinUserId, false, descriptionInputString)
-        }
-        else{
-            alert("You are not logged in!");
-        }
 
-        
+        descriptionInputString = descriptionInput.value;
+        if(descriptionInputString === ""){
+            return;
+        }
+        TODOStorage.createNewTodo(loggedinUserId, false, descriptionInputString)
+        descriptionInput.value = "";
+
+        clearPage();
     }
 
 
-    return {init}
+    return { init }
 })();
 
-function loggedInScreen(){
+function loggedInScreen() {
     const loginDivBox = document.getElementById("login-box");
+    const loggedinBox = document.getElementById("loggedin-box");
+    const logoutButton = document.getElementById("logout-button");
+    logoutButton.style.display = "block";
+    loggedinBox.style.display = "block";
     loginDivBox.style.display = "none";
-    TODOStorage.listTodos();
 }
 
-function hideCreateTodo(){
-    const loggedinBox = document.getElementById("loggedin-box");
-    loggedinBox.style.display = "block";
+
+function clearPage() {
+    const listShowTodos = document.getElementById("show-todos");
+    listShowTodos.innerHTML = "";
+    TODOStorage.listTodosByUser(loggedinUserId);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
